7292a08f671415e16aa2e145bb2dd5eb0641f9ba
wenxin.zheng <oyk4545@dingtalk.com>
2024-01-05 14:52:31 +0800

    app1.4初始化配置

---
+++ app1.4/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 设置变量
+chart_file="app1.4-1.4.0.tgz"
+release_namespace=""
+
+# 检查 release_namespace 是否为空
+if [ -z "$release_namespace" ]; then
+  echo "错误: 未设置实例命名空间(release_namespace)变量"
+  exit 1
+fi
+
+# 检查是否提供了参数
+if [ $# -ne 1 ]; then
+  echo "用法: $0 <配置文件路径>"
+  exit 1
+fi
+
+# 获取配置文件路径参数
+config_file="$1"
+
+# 提取文件名（不包括路径和扩展名）
+release_name=$(basename -s .yaml "$config_file")
+
+# 执行 Helm 安装命令
+helm install "$release_name" "$chart_file" -n "$release_namespace" -f "$config_file" 
